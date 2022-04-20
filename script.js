e99d22651173f52e8461151626247485ccc5d8d2
Tiago <ztiagok@gmail.com>
2022-04-20 18:37:10 -0500

    c2 - Condição 4 completa, 5 no meio do caminho

--- script.js
+++ script.js
@@ -4,23 +4,72 @@ function allFourColors()
   const color = document.getElementsByClassName('color')
   const backgroundColor = [color[0], color[1], color[2], color[3]]
 
+  backgroundColor[0].style.backgroundColor = "black"
+  backgroundColor[1].style.backgroundColor = "blue"
+  backgroundColor[2].style.backgroundColor = "green"
+  backgroundColor[3].style.backgroundColor = "orange"
 
-  for (let index = 0; index < backgroundColor.length; index += 1)
-  {
+  //   for (let index = 0; index < backgroundColor.length; index += 1)
+//   {
     
-    if (backgroundColor[index].style.backgroundColor === backgroundColor[index].style.backgroundColor)
+//     if (backgroundColor[index].style.backgroundColor === backgroundColor[index].style.backgroundColor)
+//     {
+
+//       backgroundColor[0].style.backgroundColor = "black"
+//       backgroundColor[1].style.backgroundColor = "blue"
+//       backgroundColor[2].style.backgroundColor = "green"
+//       backgroundColor[3].style.backgroundColor = "orange"
+//     }
+//   }
+//
+}
+
+function verifyPixelBoard()
+{
+  // FAZ ALGUMAS VERIFICAÇÕES DENTRO DO 'PIXEL-BOARD'
+
+  const element = document.querySelector('#pixel-board')
+  const child = element.children
+
+
+  for (let index = 0; index < child.length; index += 1)
+  {
+
+    if (child[index].className !== "pixel")
     {
 
-      backgroundColor[0].style.backgroundColor = "black"
-      backgroundColor[1].style.backgroundColor = "blue"
-      backgroundColor[2].style.backgroundColor = "green"
-      backgroundColor[3].style.backgroundColor = "orange"
+      child[index].className = "pixel"
     }
   }
+
+  
+
 }
+
+
+function createPixel()
+{
+  // CRIA 25 ELEMENTOS COM CLASSNAME DE "PIXEL"
+
+
+  const element = document.getElementById('pixel-board')
+
+  for (let index = 0; index < 25; index += 1)
+  {
+
+    const tag = document.createElement('div')
+    element.appendChild(tag)
+    tag.style.backgroundColor = 'white'
+    
+  }
+
+  verifyPixelBoard()
+}
+
+
 
 
 // CALL FUNCTIONS
 allFourColors()
-
+createPixel()
 // //////////////
